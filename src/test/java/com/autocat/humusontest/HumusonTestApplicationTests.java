19c1aba0d467ea880902b70ed11bc3c11fadb3f2
donghyeon-dev <donghyeondev@gmail.com>
2024-10-18 10:35:45 +0900

    initial commit

---
+++ src/test/java/com/autocat/humusontest/HumusonTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autocat.humusontest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumusonTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
